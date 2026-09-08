11007da6f72312dc00c7005093cbe2ea009d4ff1
Swarajjag10 <jagdale.swaraj03@gmail.com>
2026-09-08 09:06:19 +0530

    Fix image source path for hero photo

--- app/page.tsx
+++ app/page.tsx
@@ -91,7 +91,7 @@ export default function Home() {
 
             <img
               className="hero-photo"
-              src="/images/prajakta-about-photo.png.PNG"
+              src="/images/prajakta-about-photo.png"
               alt="Indian clinical nutritionist writing beside a balanced meal"
             />
 
